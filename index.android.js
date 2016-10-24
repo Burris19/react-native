6aed791e8eb1008c3db1fa3c820c95882cc1f5e2
Burris19 <julianhernandez19@gmail.com>
2016-10-23 23:20:04 -0700

    [add] Flex Dimensions

--- index.android.js
+++ index.android.js
@@ -7,34 +7,19 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
   View
 } from 'react-native';
 
 class LotsOfStyles extends Component {
   render() {
     return (
-      <View>
-        <Text style={styles.red}>just red</Text>
-        <Text style={styles.bigblue}>just bigblue</Text>
-        <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
-        <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  bigblue: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
-  },
-  red: {
-    color: 'red',
-  },
-});
-
 
 AppRegistry.registerComponent('MyFirstApp', () => LotsOfStyles);
