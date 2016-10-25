0965ca9ead29702819af7ebbc45dd5bf4a986c34
Burris19 <julianhernandez19@gmail.com>
2016-10-25 00:42:50 -0600

    [add] navigator basic

--- navigator/MyScene.js
+++ navigator/MyScene.js
@@ -1,17 +1,23 @@
-import React, { Component } from 'react';
-import { View, Text, Navigator } from 'react-native';
+import React, { Component, PropTypes } from 'react';
+import { View, Text, TouchableHighlight } from 'react-native';
 
 export default class MyScene extends Component {
-  static get defaultProps() {
-    return {
-      title: 'MyScene'
-    };
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    onForward: PropTypes.func.isRequired,
+    onBack: PropTypes.func.isRequired,
   }
 
-  render() {
+  render(){
     return (
       <View>
-        <Text>Hi! My name is {this.props.title}</Text>
+        <Text>Current Scene: { this.props.title }</Text>
+        <TouchableHighlight onPress={this.props.onForward}>
+          <Text>Tap me to load the next scene</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={this.props.onBack}>
+          <Text>Tap me to go back</Text>
+        </TouchableHighlight>
       </View>
     );
   }
