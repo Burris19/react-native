b6c057edb26038d28599524f7bad7ef91153e080
Burris19 <julianhernandez19@gmail.com>
2016-10-24 19:43:53 -0700

    [add] Justify Content #

--- index.android.js
+++ index.android.js
@@ -13,7 +13,11 @@ import {
 class LotsOfStyles extends Component {
   render() {
     return (
-      <View style={{flex: 1, flexDirection: 'row'}}>
+      <View style={{
+          flex: 1,
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+        }}>
         <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
         <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
         <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
