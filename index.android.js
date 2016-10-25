e9eab07b5e4ce443994b11c684c62f2da4466170
Burris19 <julianhernandez19@gmail.com>
2016-10-24 20:09:17 -0700

    [add] handling-text-input.html

--- index.android.js
+++ index.android.js
@@ -7,23 +7,31 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  Text,
+  TextInput,
   View
 } from 'react-native';
 
-class LotsOfStyles extends Component {
+class PizzaTranslator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+
   render() {
     return (
-      <View style={{
-          flex: 1,
-          flexDirection: 'column',
-          justifyContent: 'space-between',
-        }}>
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+      <View style={{padding: 10}}>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Type here to translate!!"
+          onChangeText={(text) => this.setState({text})}
+        />
+        <Text style={{padding: 10, fontSize: 42}}>
+          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+        </Text>
       </View>
     );
   }
 }
 
-AppRegistry.registerComponent('MyFirstApp', () => LotsOfStyles);
+AppRegistry.registerComponent('MyFirstApp', () => PizzaTranslator);
